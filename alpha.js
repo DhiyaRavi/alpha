9f69298ffb5a832b5a976b29cf26406ad2281199
DhiyaRavi <sandhiyaravindran5@gmail.com>
2025-05-05 23:28:29 +0530

    Add files via upload

---
+++ alpha.js
@@ -0,0 +1,117 @@
+var total=document.querySelectorAll(".alpha").length;
+for(i=0;i<total;i++){
+document.querySelectorAll(".alpha")[i].addEventListener("click",sound)
+function sound(){
+    var buttonClick=this.innerHTML
+    switch(buttonClick)
+    {
+        case "A":
+            var audio=new Audio("Audio/A_a.opus");
+            audio.play();
+            break;
+            case "B":
+                var audio=new Audio("Audio/B_a.opus");
+                audio.play();
+                break;
+                case "C":
+                    var audio=new Audio("Audio/C_a.opus");
+                    audio.play();
+                    break;
+                    case "D":
+                        var audio=new Audio("Audio/D_a.opus");
+                        audio.play();
+                        break;
+                        case "E":
+                            var audio=new Audio("Audio/E_a.opus");
+                            audio.play();
+                            break;
+                            case "F":
+                                var audio=new Audio("Audio/F_a.opus");
+                                audio.play();
+                                break;
+                                case "G":
+                                var audio=new Audio("Audio/G_sa.opus");
+                                audio.play();
+                                break;
+                                case "H":
+                                    var audio=new Audio("Audio/H_a.opus");
+                                    audio.play();
+                                    break; 
+                                    case "I":
+                                        var audio=new Audio("Audio/I_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "J":
+                                        var audio=new Audio("Audio/J_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "K":
+                                            var audio=new Audio("Audio/K_a.opus");
+                                            audio.play();
+                                            break; 
+                                            case "L":
+                                             var audio=new Audio("Audio/L_a.opus");
+                                             audio.play();
+                                              break;
+                                        case "M":
+                                        var audio=new Audio("Audio/M_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "N":
+                                        var audio=new Audio("Audio/N_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "O":
+                                        var audio=new Audio("Audio/O_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "P":
+                                        var audio=new Audio("Audio/P_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "Q":
+                                        var audio=new Audio("Audio/Q_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "R":
+                                        var audio=new Audio("Audio/R_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "S":
+                                        var audio=new Audio("Audio/S_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "T":
+                                        var audio=new Audio("Audio/tt_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "U":
+                                        var audio=new Audio("Audio/U_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "V":
+                                        var audio=new Audio("Audio/V_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "W":
+                                        var audio=new Audio("Audio/W_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "X":
+                                        var audio=new Audio("Audio/X_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "Y":
+                                        var audio=new Audio("Audio/Y_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "Z":
+                                        var audio=new Audio("Audio/Z_a.opus");
+                                        audio.play();
+                                        break;
+
+    } 
+
+
+    }
+}
